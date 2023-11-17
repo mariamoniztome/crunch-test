2384434744bdfa3faa436f01933150a7f493b7f1
mariamoniztome <mjtome12@gmail.com>
2023-11-17 09:30:37 +0000

    Create Reference form component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 /**
  * The Home component renders the main content of the home page.
- * It includes the HeroSection, Grid, and HeadsUpNotification components.
+ * It includes the HeroSection, Grid, Reference and HeadsUpNotification components.
  * @returns The rendered Home component.
  */
 import HeroSection from "../components/herosection/HeroSection";
 import Grid from "../components/grid/Grid";
 import HeadsUpNotification from "../components/headsupnotification/HeadsUpNotification.";
+import Reference from "../components/reference/Reference";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
         <HeroSection />
         <Grid />
         <HeadsUpNotification />
+        <Reference />
     </main>
       
 
